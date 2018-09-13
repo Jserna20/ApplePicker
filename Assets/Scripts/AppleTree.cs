d047b42e845b71c7111e988bf64d9b87f0f28c7e
Jserna20 <31867660+Jserna20@users.noreply.github.com>
2018-09-13 19:06:46 -0400

    Instantiating the Baskets
    Created the Basket Object

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppleTree : MonoBehaviour {
+public class AppleTree : MonoBehaviour
+{
     [Header("Set in Inspector")]
 
     // Pefab for instantiating apples
