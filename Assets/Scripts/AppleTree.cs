acbfef9395dffcea544758f68026a7e18acd4dfb
Jserna20 <31867660+Jserna20@users.noreply.github.com>
2018-09-13 17:16:13 -0400

    Basic Movement & Changing Direction

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour {
+    [Header("Set in Inspector")]
+
+    // Pefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    // Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 1f;
+
+    // Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+	
+	void Start () //Dropping apples every second
+    {
+        
+	}
+	
+	void Update () 
+    {
+        // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Changing Direction
+        if (pos.x < -leftAndRightEdge)
+            speed = Mathf.Abs(speed); // Move right
+        else if (pos.x > leftAndRightEdge)
+            speed = -Mathf.Abs(speed); // Move left
+    }
+}
