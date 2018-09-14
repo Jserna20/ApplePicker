ca51882a422de186f83dcb6fa2130736bc5cb25d
Jserna20 <31867660+Jserna20@users.noreply.github.com>
2018-09-13 20:32:45 -0400

    Notifying Apple Picker of apples
    Is told when apple is destroyed and will now destroy 1 basket for every apple that is dropped

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dymanically")]
+    public Text scoreGT;
 
-	// Use this for initialization
 	void Start ()
     {
-		
+        // Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        // Get the Text Component of that GameObject
+        scoreGT = scoreGO.GetComponent<Text>();
+        // Set the starting number of points to 0
+        scoreGT.text = "0";
 	}
 	
-	// Update is called once per frame
 	void Update ()
     {
         // Get the current screen position of the mouse from Input
@@ -34,6 +40,18 @@ public class Basket : MonoBehaviour
         //Find out what hit this basket
         GameObject collidedWith = coll.gameObject;
         if (collidedWith.tag == "Apple")
+        {
             Destroy(collidedWith);
+
+            //Parse the text of the scoreGt into an int
+            int score = int.Parse(scoreGT.text);
+            //Add points for catching the apple
+            score += 100;
+            //Convert the score back to a string and display it
+            scoreGT.text = score.ToString();
+        }
+            
+
+
     }
 }
